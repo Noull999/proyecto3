6f5d574152c71a8a150c0dc5d7fcc84b96a9592a
jose <joseestebanasencio@gmail.com>
2024-02-26 22:45:07 -0300

    pagina web y chart

--- app.js
+++ app.js
@@ -23,4 +23,36 @@ function displayWeatherData(data) {
         <p>Viento: ${data.wind.speed} m/s</p>
     `;
 }
+function createChart(data) {
+    new Chart(chartContainer, {
+        type: 'bar',
+        data: {
+            labels: ['Temperatura', 'Sensación térmica', 'Humedad', 'Viento'],
+            datasets: [{
+                label: 'Valores',
+                data: [data.main.temp, data.main.feels_like, data.main.humidity, data.wind.speed],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
 
